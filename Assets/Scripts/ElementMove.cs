5473327a71f73573a2de5359359fd7156ab19d41
MPerezMateo <m.perez.mateo@hotmail.com>
2024-02-17 21:14:35 +0100

    Correcton to A* algorithm
    Added a height limitation functionality to the A* path, fixed initialization of attributes.
    Some other minor fixes and optimizatioons, passed array to list, correct search of items, update of valid positions.

--- Assets/Scripts/ElementMove.cs
+++ Assets/Scripts/ElementMove.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class Element : Moveable
 {
   // Start is called before the first frame update
-  void Start()
+  protected new void Start()
   {
+    base.Start();
   }
 
   // Update is called once per frame
--- Assets/Scripts/Moveable.cs
+++ Assets/Scripts/Moveable.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public abstract class Moveable : MonoBehaviour
 {
-  public int movementRange = 6;
-  public double jumpReach = .5;
+  public int movementRange;
+  public double jumpReach;
+
+  protected void Start()
+  {
+    // We have to set default values here, in the declaration they do not inherit anything
+    movementRange = 7;
+    jumpReach = .5;
+  }
   protected void move()
   {
     if (Input.GetMouseButtonDown(0) && GlobalVariables.selection == gameObject && GlobalVariables.highlight != null && GlobalVariables.highlight != gameObject && GlobalVariables.highlight.tag == "Pathable")
@@ -27,11 +34,13 @@ public abstract class Moveable : MonoBehaviour
   {
     List<GameObject> pathables = GameObject.FindGameObjectsWithTag("Pathable") // Esto debe ser pathable en un futuro
                                        .Where(p => Math.Abs(p.transform.position.x - transform.position.x) + Math.Abs(p.transform.position.y - transform.position.y) <= movementRange)
+                                       .OrderBy(p => p.name)
                                        .ToList();
+
     GameObject origin = pathables.Find(p => p.transform.position.x == player.transform.position.x && p.transform.position.y == player.transform.position.y);
     //Debug.Log(player.transform.position);
     //Debug.Log(origin.transform.position);
-    int[] visited = pathables.Select(_ => -1).ToArray();
+    List<int> visited = pathables.Select(_ => -1).ToList();
     int initialIndex = Enumerable.Range(0, pathables.Count)
                 .FirstOrDefault(i => pathables[i].transform.position.x == origin.transform.position.x && pathables[i].transform.position.y == origin.transform.position.y);
     visited[initialIndex] = 0;
@@ -44,60 +53,73 @@ public abstract class Moveable : MonoBehaviour
     // Ubicamos los pesos de distancias en nuestra matriz de paso
     setDistance(pathables, visited);
     // Hallamos el camino óptimo sobre los pesos de nuestra matriz de paso
+    if (!pathables.Contains(destination))
+    {
+      Debug.Log("Error, destination or origin not reachable");
+      return;
+    }
     List<GameObject> way = setPath(pathables, visited, destination);
     foreach (var tile in way)
       Debug.Log(tile.transform.position);
   }
 
-  bool TestDirection(GameObject obj, List<GameObject> space, int[] visited, int step, Vector2 direction)
+  bool TestDirection(GameObject origin, List<GameObject> space, List<int> visited, int step, int destIndex)
   {
-    int destinationIndex = Enumerable.Range(0, space.Count)
-                .FirstOrDefault(i => space[i].transform.position.x == obj.transform.position.x + direction.x && space[i].transform.position.y == obj.transform.position.y + direction.y);
-    // Aqui poner la cuestión de la altura en el if 
-    if (space.FirstOrDefault(e => e.transform.position.x == obj.transform.position.x + direction.x && e.transform.position.y == obj.transform.position.y + direction.y) != null
-        && visited[destinationIndex] == step) // && Math.Abs(transform.position.z - space[destinationIndex].transform.position.z) <= jumpReach
+    //Debug.Log($"Distance between {origin} and {space[destIndex]} is {Math.Abs((origin.transform.position.z + 0.5 * origin.transform.localScale.z) - (space[destIndex].transform.position.z + 0.5 * space[destIndex].transform.localScale.z))}");
+    if (destIndex != -1 && visited[destIndex] == step
+         && Math.Abs((origin.transform.position.z + 0.5 * origin.transform.localScale.z) - (space[destIndex].transform.position.z + 0.5 * space[destIndex].transform.localScale.z)) <= jumpReach)
       return true;
     return false;
   }
 
-  void TestFourDirections(GameObject obj, List<GameObject> space, int[] visited, int step)
+  void TestFourDirections(GameObject origin, List<GameObject> space, List<int> visited, int step)
   {
     Vector2[] cross = { new Vector2(0, 1), new Vector2(0, -1), new Vector2(1, 0), new Vector2(-1, 0) };
     int destinationIndex;
     foreach (var direction in cross)
     {
-      if (TestDirection(obj, space, visited, -1, direction))
+      destinationIndex = space.FindIndex(e => e.transform.position.x == origin.transform.position.x + direction.x && e.transform.position.y == origin.transform.position.y + direction.y);
+      if (TestDirection(origin, space, visited, -1, destinationIndex))
       {
-        destinationIndex = Enumerable.Range(0, space.Count)
-                .FirstOrDefault(i => space[i].transform.position.x == obj.transform.position.x + direction.x && space[i].transform.position.y == obj.transform.position.y + direction.y);
         visited[destinationIndex] = step;
       }
     }
   }
-  void setDistance(List<GameObject> space, int[] visited)
+  void setDistance(List<GameObject> space, List<int> visited)
   {
     int index;
     for (int step = 1; step < space.Count; step++)
     {
       foreach (GameObject obj in space)
       {
-        index = Enumerable.Range(0, space.Count)
-              .FirstOrDefault(i => space[i].transform.position.x == obj.transform.position.x && space[i].transform.position.y == obj.transform.position.y);
+        // We find the index of our object
+        index = space.FindIndex(e => e.transform.position.x == obj.transform.position.x && e.transform.position.y == obj.transform.position.y);
         if (visited[index] == step - 1)
           TestFourDirections(obj, space, visited, step);
       }
     }
+
+    // Aqui devolver todos los pathables distintos de -1
+    // En un futuro, al seleccionar a nuestro movible, debemos resaltar las áreas posibles.
+
+    for (int i = visited.Count - 1; i >= 0; i--)
+    {
+      if (visited[i] == -1)
+      {
+        space.RemoveAt(i);
+        visited.RemoveAt(i);
+      }
+    }
   }
 
-  List<GameObject> setPath(List<GameObject> space, int[] visited, GameObject destination)
+  List<GameObject> setPath(List<GameObject> space, List<int> visited, GameObject destination)
   {
     List<GameObject> path = new List<GameObject>();
     List<GameObject> temp = new List<GameObject>();
     int step;
-    int indexDestination = Enumerable.Range(0, space.Count)
-              .FirstOrDefault(i => space[i].transform.position.x == destination.transform.position.x && space[i].transform.position.y == destination.transform.position.y);
+    int indexDestination = space.FindIndex(e => e.transform.position.x == destination.transform.position.x && e.transform.position.y == destination.transform.position.y);
 
-    if (visited[indexDestination] > 0)
+    if (visited[indexDestination] > 0 && visited[indexDestination] <= movementRange)
     {
       path.Add(destination);
       step = visited[indexDestination] - 1;
@@ -105,19 +127,18 @@ public abstract class Moveable : MonoBehaviour
     else
     {
       Debug.Log("Can't reach desired location");
-      return null;
+      return path;
     }
     Vector2[] cross = { new Vector2(0, 1), new Vector2(0, -1), new Vector2(1, 0), new Vector2(-1, 0) };
-    int destinationIndex;
+    int newPos;
     for (int i = step; step > -1; step--)
     {
       foreach (var dir in cross)
       {
-        destinationIndex = Enumerable.Range(0, space.Count)
-                .FirstOrDefault(i => space[i].transform.position.x == destination.transform.position.x + dir.x && space[i].transform.position.y == destination.transform.position.y + dir.y);
-        // Esta parte está mal
-        if (TestDirection(destination, space, visited, step, dir))
-          temp.Add(space[destinationIndex]);
+        newPos = space.FindIndex(e =>
+         e.transform.position.x == destination.transform.position.x + dir.x && e.transform.position.y == destination.transform.position.y + dir.y);
+        if (TestDirection(destination, space, visited, step, newPos))
+          temp.Add(space[newPos]);
       }
       GameObject tempObj = FindClosest(destination, temp);
       path.Add(tempObj);
@@ -131,7 +152,7 @@ public abstract class Moveable : MonoBehaviour
   GameObject FindClosest(GameObject destination, List<GameObject> list)
   {
     float currentDistance = float.PositiveInfinity;
-    int indexNumber = 0;
+    int indexNumber = -1;
     float dist;
     for (int i = 0; i < list.Count; i++)
     {
@@ -142,6 +163,6 @@ public abstract class Moveable : MonoBehaviour
         indexNumber = i;
       }
     }
-    return list[indexNumber];
+    return indexNumber != -1 ? list[indexNumber] : null;
   }
 }
